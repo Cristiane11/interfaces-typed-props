95e5933b63f0ab0e511689ca44b0a1797986c302
Cristiane Abreu <bjwmcd@yahoo.com>
2025-10-07 16:28:16 -0400

    update

--- interfaces-typed-props/src/App.tsx
+++ interfaces-typed-props/src/App.tsx
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { Person, ProfileProps } from './models/Person.model';
 import PersonComponent from './components/PersonComponent';
+import Status from './components/Status';
 import Card from 'react-bootstrap/Card';
 
-function App() {
+function App(){
   const student: Person = {
     name: 'John',
     age: 25,
@@ -16,12 +17,17 @@ function App() {
     message: 'This is a message from the parent component.'
   }
 
-  console.log({profileData});
-/* added code stops */
+
 
   return (
     <div className="container p-3">
-      <h1>{student.name}</h1>
+      
+       <Card>
+        <Card.Body>
+          <PersonComponent {...profileData}/>
+        </Card.Body>
+      </Card>
+      <Status/>
     
     </div>
   );
